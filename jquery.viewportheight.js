51b751e58d0e617e5c1c6ea664c92f01fa7744f8
Sebastián Grignoli <grignoli@gmail.com>
2015-06-18 21:01:52 -0300

    Initial version

---
+++ jquery.viewportheight.js
@@ -0,0 +1,55 @@
+(function(){
+  var watch = [];
+  var interval = false;
+  var onresizeloaded = false;
+
+  var resizer = function(){
+      if(watch.length){
+        for( i in watch){
+          if(watch[i].percent) watch[i].$elm.height(window.innerHeight*(watch[i].percent/100));
+          else if(watch[i].param) watch[i].$elm.height(window.innerHeight+watch[i].param);
+          else watch[i].$elm.height(0);
+        }
+      } else {
+        clearInterval(interval);
+      }
+    };
+
+  jQuery.fn.setViewportRelativeHeight = function(param){
+    var elms = jQuery(this);
+    elms.each(
+      function(i){
+        var percent = 0;
+        if(typeof(param) == 'string' && param.indexOf('%')){
+          percent = param.split('%')[0];
+          param = "";
+        } else if(typeof(param) == 'undefined'){
+          percent = 100;
+          param = "";
+        } else {
+          param = parseFloat(param);
+        }
+        watch.push({'$elm': jQuery(this), 'param': param, 'percent': percent});
+      }
+    )
+
+    // adjust size right now
+    resizer();
+
+    // adjust size again when page load completes
+    jQuery(resizer);
+
+    // sometimes the page did not completely load when load event occurs due
+    // to delayed asynchronous ajax calls or lazy image load
+    // so, start watching with a timer, but stop after 10 seconds
+    if(!interval) {
+      interval = setInterval(resizer, 500);
+      setTimeout(function(){ clearInterval(interval); interval = false; }, 10000);
+    }
+
+    // also start listening to the onresize event of the window
+    if(!onresizeloaded) jQuery(window).resize(resizer); onresizeloaded = true;
+
+    return elms;
+  }
+})();
